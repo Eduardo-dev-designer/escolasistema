41ab9433b40e4a80dcdef3cc9063867a0fade392
Eduardo <eduardomesquita656@gmail.com>
2026-09-08 21:31:56 -0300

    escolaaaa

--- iscola/AvaliacaoJava-master/src/main/java/Test_package/Avaliacaotest.java
+++ iscola/AvaliacaoJava-master/src/main/java/Test_package/Avaliacaotest.java
@@ -1,7 +1,7 @@
 package Test_package;
 
 import model.Avaliacao;
-import org.junit.jupiter.api.Test;
+import  org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
